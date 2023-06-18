e73c7e06643f81d60b7f94f9efa7434ecb8e51ce
mkandan1 <64264387+mkandan1@users.noreply.github.com>
2023-06-18 22:32:48 +0530

    Add login, signup, home, bootstrap CDN

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/login' element={< Login />}></Route>
+          <Route exact path='/signup' element={< Signup />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
