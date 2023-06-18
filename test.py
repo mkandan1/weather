385f98dc42f1469767c0bc879c6f3cd4b6cf2105
mkandan1 <64264387+mkandan1@users.noreply.github.com>
2023-06-18 21:33:37 +0530

    Basic model

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import python_weather
 import asyncio
 import os
 import pyttsx3
 
 app = Flask(__name__)
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
 
 async def get_weather(city):
     async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
@@ -18,33 +15,29 @@ async def get_weather(city):
 def home():
     return render_template('index.html')
 
-@app.route('/weather.html')
+@app.route('/fetchReport', methods=['GET', 'POST'])
 def weather():
-    city = request.args.get('city')
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    weather = loop.run_until_complete(get_weather(city))
-    loop.close()
+    if request.method == 'POST':
+        city = request.get_json()['city']
+        print(city)
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        weather = loop.run_until_complete(get_weather(city))
+        loop.close()
 
-    if weather:
-        temperature = f"Temperature: {weather.current.temperature}°F"
-        description = f"Description: {weather.current.description}"
-        uv_index = f"Ultra violet: {weather.current.ultraviolet}UV"
-        humidity = f"Humidity: {weather.current.humidity} g/kg"
-        feelslike = f"Feels Like: {weather.current.feels_like}"
-        wind_speed_ = f"Wind Speed: {weather.current.wind_speed} km/h"
-        
-        # Say the weather conditions
-        engine.say(f"Searching weather conditions in {city}")
-        engine.say(temperature)
-        engine.say(description)
-        engine.say(humidity)
-        engine.runAndWait()
-        
-        return render_template('weather.html', city=city, temperature=temperature, description=description, uv_index=uv_index, humidity=humidity, feelslike=feelslike, wind_speed_=wind_speed_)
+        if weather:
+            temperature = f"{weather.current.temperature}°F"
+            description = f"{weather.current.description}"
+            uv_index = f"{weather.current.ultraviolet}UV"
+            humidity = f"{weather.current.humidity} g/kg"
+            feelslike = f"{weather.current.feels_like}"
+            wind_speed_ = f"{weather.current.wind_speed} km/h"
+            res = {"city": city, "temperature":temperature, "description":description, "uv_index": uv_index, "humidity":humidity, "feelslike":feelslike, "wind_speed_":wind_speed_}
+            print(res)
+            return jsonify(res)
 
-    else:
-        return render_template('weather.html', city=city, error=True)
+        else:
+            return render_template('weather.html', city=city, error=True)
 
 
 if __name__ == '__main__':
