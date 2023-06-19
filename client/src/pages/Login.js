df01939f5d15d59e50f8484fb5881cd633b08535
Cobra004 <elamaran.infotec@gmail.com>
2023-06-19 06:01:29 +0100

    login with bootstrap

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
+
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+    <Form/>
+    </>
+  )
+}
+
+function Form(){
+  return(
+    <>
+    <form className='form m-5'>
+    <center>
+    <p class="h1"><b>LOGIN</b></p>
+    <br></br>
+    <p class="h4">Hello again!</p>
+    </center>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </>
   )
 }
