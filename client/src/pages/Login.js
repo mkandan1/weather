5215fa2e7b58b499765ac408e820aa2129cb9893
_mad_maran_ <elamaran.infotech@gmail.com>
2023-06-20 08:01:37 +0100

    sign
    sign

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -32,12 +32,12 @@ function Form() {
           <p className="h4">Hello again!</p>
         </center>
         <div className={`mb-3 ${validated ? 'was-validated' : ''}`}>
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <label htmlFor="exampleInputEmail1" className="form-label"><b>Email address</b></label>
           <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className={`mb-3 ${validated ? 'was-validated' : ''}`}>
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <label htmlFor="exampleInputPassword1" className="form-label"><b>Password</b></label>
           <input type="password" className="form-control" id="exampleInputPassword1" required />
         </div>
         <div className="mb-3 form-check">
